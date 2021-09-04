f4a133b0bae635de2c25fe692855a134072fc8d7
rafael <rafaelcostasantana13@gmail.com>
2021-09-03 21:22:02 -0300

    todos numeros ate o 1000000

---
+++ main.c
@@ -0,0 +1,13 @@
+///Todos os numeros ate o numero escolhido no cod
+#include <stdio.h>
+#include <stdio.h>
+int main(){
+    int contador = 1;
+/// lembre de mudar vvvvvvv
+ while (contador <= 100000) {
+/// lembre de mudar ^^^^^^^
+     printf("%d\n",contador);
+  contador++;
+   }
+    return 0;
+}
